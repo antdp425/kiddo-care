7b1021c87db99d0b3917d25ca0f14631dcea2ff3
antdp425 <antdp425@aol.com>
2020-05-10 03:09:49 -0400

    adds validation for user

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,4 @@
 class Children < ActiveRecord::Base 
    belongs_to :user
+   validates :first_name, :last_name, :contact_phone, :contact_email, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base 
    has_secure_password
    has_many :children
+   validates :email, :username, :password, presence: true
+   validates :username, uniqueness: { case_sensitive: false }
 end
