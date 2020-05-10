34a7fd085a4b13c921e7e955390fad59722a8ba3
antdp425 <antdp425@aol.com>
2020-05-10 13:46:07 -0400

    adds signup functionality as well as dashboard skeleton

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
    
+   get '/dashboard' do
+      if logged_in?
+         erb :"users/dashboard"
+      else
+         redirect '/'
+      end
+   end
    
+   get '/signup' do
+      if logged_in?
+         redirect '/dashboard'
+      else
+         erb :"users/signup"
+      end
+   end
+
+   post '/signup' do
+      user = User.new(params)
+      if user.save
+         session[:user_id] = user.id
+         redirect '/dashboard'
+      else
+         redirect '/signup'
+      end
+   end
 
 end
