428386819dcc1fd4c3bd878be262bcd101dbdf81
antdp425 <antdp425@aol.com>
2020-05-10 16:04:35 -0400

    adds get '/children/new' + post '/children' + children/new view

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -10,8 +10,26 @@ class ChildrenController < ApplicationController
    end
 
    get '/children/new' do
+      binding.pry
       if logged_in?
          erb :'children/new'
+      else
+         redirect '/'
+      end
+   end
+
+   post '/children' do
+      child = current_user.children.new(params)
+      if child.save
+         redirect "/children/#{child.id}"
+      else
+         redirect '/children/new'
+      end
+   end
+
+   get '/children/:id' do
+      if logged_in?
+         erb :'children/show'
       else
          redirect '/'
       end
