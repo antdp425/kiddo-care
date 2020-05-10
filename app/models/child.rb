922c488672495970b62400bc44e363af8efe2ad6
antdp425 <antdp425@aol.com>
2020-05-10 04:31:48 -0400

    adds addt'l validation for Child contact_phone

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,5 +1,5 @@
 class Child < ActiveRecord::Base 
    belongs_to :user
    validates :first_name, :last_name, :contact_phone, :contact_email, presence: true
-   validates :contact_phone, length: {maximum: 10}
+   validates :contact_phone, length: {maximum: 10}, numericality: true
 end
