19645f8a8bc30dbcd6fd19d78fa8e223a74fc52a
antdp425 <antdp425@aol.com>
2020-05-10 15:28:34 -0400

    adds children index

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,3 +1,19 @@
 class ChildrenController < ApplicationController
-   
+
+   get '/children' do
+      if logged_in?
+         @user = current_user
+         erb :'children/index'
+      else
+         redirect '/'
+      end
+   end
+
+   get '/children/new' do
+      if logged_in?
+         erb :'children/new'
+      else
+         redirect '/'
+      end
+   end
 end
