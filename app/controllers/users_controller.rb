c177eb316f10ed4f70fc323ba14d86e75323381f
antdp425 <antdp425@aol.com>
2020-05-10 14:44:54 -0400

    adds login/logout functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,4 +26,27 @@ class UsersController < ApplicationController
       end
    end
 
+   get '/login' do
+      if logged_in?
+         redirect '/dashboard'
+      else
+         erb :"users/login"
+      end
+   end
+
+   post '/login' do
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+         session[:user_id] = user.id
+         redirect '/dashboard'
+      else
+         redirect '/login'
+      end
+   end
+
+   get '/logout' do
+      session.clear
+      redirect '/'
+   end
+
 end
