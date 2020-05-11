adca0f748a566722ca533d1f7a0a7ea4af75c222
antdp425 <antdp425@aol.com>
2020-05-11 01:34:22 -0400

    adds user logged_in? checks for post/patch/delete request

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -17,11 +17,15 @@ class ChildrenController < ApplicationController
    end
 
    post '/children' do
-      child = current_user.children.new(params)
-      if child.save
-         redirect "/children/#{child.id}"
+      if logged_in?
+         child = current_user.children.new(params)
+         if child.save
+            redirect "/children/#{child.id}"
+         else
+            redirect '/children/new'
+         end
       else
-         redirect '/children/new'
+         redirect '/'
       end
    end
 
@@ -52,21 +56,27 @@ class ChildrenController < ApplicationController
    end
 
    patch '/children/:id' do
-      child = current_user.children.find_by(id: params[:id])
-      if child.update(params.except(:id, :_method))
-         redirect "/children/#{child.id}"
+      if logged_in?
+         child = current_user.children.find_by(id: params[:id])
+         if child.update(params.except(:id, :_method))
+            redirect "/children/#{child.id}"
+         else
+            redirect "/children/#{child.id}/edit"
+         end
       else
-         redirect "/children/#{child.id}/edit"
+         redirect '/'
       end
    end
 
    delete '/children/:id' do
-      child = current_user.children.find_by(id: params[:id])
-      if child
-         child.delete
-         redirect '/children'
+      if logged_in?
+         child = current_user.children.find_by(id: params[:id])
+         if child
+            child.destroy
+            redirect '/children'
+         end
       else
-         redirect '/children'
+         redirect '/'
       end
    end
 
