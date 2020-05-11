a486e182a1bad9a94c66879fc5e540b978bb7e63
antdp425 <antdp425@aol.com>
2020-05-11 08:19:42 -0400

    adds first + last name fields to sign up form & removes for attribute in signup labels

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base 
    has_secure_password
    has_many :children
-   validates :email, :username, :password, presence: true
+   validates :first_name, :last_name, :email, :username, :password, presence: true
    validates :username, uniqueness: { case_sensitive: false }
 end
