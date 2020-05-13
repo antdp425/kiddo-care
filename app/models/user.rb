3c7401349b227ed335df6d34996377bf6c4fa173
antdp425 <antdp425@aol.com>
2020-05-13 10:36:51 -0400

    adds styling for errors + addt'l validation for Users (first_name, last_name, username)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
    has_many :children
    has_many :employees
    validates :first_name, :last_name, :email, :username, :password, presence: true
+   validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/,
+   message: "only allows letters" }
    validates :username, uniqueness: { case_sensitive: false }
+   validates :username, format: { with: /[a-zA-Z0-9]/, message: "can only include letters and numbers"}
 end
