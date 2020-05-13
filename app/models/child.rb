d816c0baa30d77358e4aa20cabda975d90328dd2
antdp425 <antdp425@aol.com>
2020-05-13 09:00:42 -0400

    adds addt'l validation for first_name & last_name in Employee + Child models -- also adds addt'l validtions for contact_phone and phone in Employee + Child models respectively

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,5 +1,7 @@
 class Child < ActiveRecord::Base 
    belongs_to :user
    validates :first_name, :last_name, :contact_phone, :contact_email, presence: true
-   validates :contact_phone, length: {is: 10}, numericality: true
+   validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/,
+   message: "only allows letters" }
+   validates :contact_phone, length: {is: 10}, numericality: { only_integer: true, message: "only allows numbers"}
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,7 @@
 class Employee < ActiveRecord::Base 
    belongs_to :user
    validates :first_name, :last_name, :phone, :email, presence: true
-   validates :phone, length: {is: 10}, numericality: true
+   validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/,
+   message: "only allows letters" }
+   validates :phone, length: {is: 10}, numericality: { only_integer: true , message: "only allows numbers" }
 end
