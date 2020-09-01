8bfd099000f47bf9138f3b38bfb653ab856300b6
antdp425 <antdp425@aol.com>
2020-09-01 04:35:37 -0400

    trying to resolve internal error on heroku

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,20 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
-  # :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+ 
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+ end
 
 require './app/controllers/application_controller'
 require_all 'app'
